4df0a62ccf6a4d1bc013a6502cd1fbe8a4ab66d4
JAC-EE <jack.clayton2001@hotmail.com>
2024-07-06 22:11:51 +1000

    Fixed pinout in info.yaml. Working on mux cocotb test. Currently non-functional.

--- test/test.py
+++ test/test.py
@@ -1,6 +1,8 @@
 # SPDX-FileCopyrightText: © 2024 Tiny Tapeout
 # SPDX-License-Identifier: Apache-2.0
 
+# Don't use python for HDL test benches. It is terrible.
+
 # ToDo:
 # COMPLETE Fix bit setting in SPI_send
 # Implement testing for ScreenSel, keyplxr, and miso
@@ -56,6 +58,7 @@ async def test_project(dut):
     # Load test values from CSV file
     seven_segment_anode = read_test_values('7_segment_anode.csv')
     ScreenSelect_test = read_test_values('screenselect.csv')
+    multiplexer = read_test_values('multiplexer.csv')
 
     # Set the clock period
     clock = Clock(dut.clk, clock_period, units="us")
@@ -87,7 +90,7 @@ async def test_project(dut):
             dut.ui_in.value = int(dut.ui_in.value) | 0x4 # SPI enable
             #await ClockCycles(dut.clk, 1)
             await Timer(clock_period, units='us') 
-            await SPI_send(dut, ScreenSelect_test[c][0] | i, 8, 2) #send loop iteration, 8 bits, bit mask 0000 0010
+            await SPI_send(dut, c<<4 | i, 8, 2) #send loop iteration, 8 bits, bit mask 0000 0010 # note c<<4 was ScreenSelect_test[c][0] for some reason
             dut.ui_in.value = int(dut.ui_in.value) & ~0x4 # SPI disable
             #await ClockCycles(dut.clk, 1)
             dut.rst_n.value = 0 # Activate reset (prevents SCK functioning) DEBUG
@@ -103,6 +106,7 @@ async def test_project(dut):
                 dut._log.error(str(e))
                 errors += 1
             # END 7 Segment display logic test
+            
             # Test ScreenSel logic
             try:
                 assert dut.uio_out.value & 0xF == ScreenSelect_test[c][1], f"Screen Select result incorrect: Was: {hex(dut.uio_out.value & 0xF)} Should be: {hex(ScreenSelect_test[i][1])}" #ScreenSel test
@@ -110,6 +114,33 @@ async def test_project(dut):
                 dut._log.error(str(e))
                 errors += 1
             # END Screensel logic test
+            
+            # Test Multiplexer logic
+            # Test mux is off
+            try:
+                assert dut.uo_out.value & 0x80 == 1<<7, f"Multiplexer result 1 incorrect: Was: {hex(int(dut.uo_out.value & 0x80)>>7)} Should be: {hex(1)}" #Multiplexer off test (active low)
+            except AssertionError as e:
+                dut._log.error(str(e))
+                errors += 1
+                
+            for x in range(0xF+1): #+1 because python is gross
+                dut.ui_in.value = int((dut.ui_in.value) | (x<<4)) & 0xF0 # Turn x<<4 on. Same 0xF0 as in range(0xF0)
+                await Timer(0.5*clock_period, units='us') #half step
+                try:
+                    assert dut.uo_out.value & 0x80 == ~((multiplexer[c>>2][1] & x)>>(c>>2)), f"Multiplexer result 2 incorrect: Was: {hex(int(dut.uo_out.value & 0x80)>>7)} Should be: {bin(((~((multiplexer[c>>2][1]&0xFF) & x)&0xF)>>(4-(c>>2)))&0x1)} - c:{bin(c)} - M&x:{bin(multiplexer[c>>2][1] & x)} - x:{bin(x)} - M:{bin(multiplexer[c>>2][1] & 0xF)} - {bin(4-(c>>2))}. Iteration: {hex(x<<4)}" #Multiplexer off test (active low)
+                except AssertionError as e:
+                    dut._log.error(str(e))
+                    errors += 1
+                await Timer(0.5*clock_period, units='us') #half step
+                dut.ui_in.value = int((dut.ui_in.value) & ~(x<<4)) & 0xF0 # Turn x<<4 off. Same 0xF0 as in range(0xF0)
+                await Timer(0.5*clock_period, units='us') #half step
+                try:
+                    assert dut.uo_out.value & 0x80 == 1<<7, f"Multiplexer result 3 incorrect: Was: {hex(int(dut.uo_out.value & 0x80)>>7)} Should be: {hex(1)}. Iteration: {hex(x<<4)}" #Multiplexer off test (active low)
+                except AssertionError as e:
+                    dut._log.error(str(e))
+                    errors += 1
+                await Timer(0.5*clock_period, units='us') #half step
+            # END Multiplexer logic test
             
             dut.ui_in.value = int(dut.ui_in.value) & ~0x4 # SPI disable
             dut.rst_n.value = 1 # Deactivate reset (returns SCK to functioning) DEBUG
